896a0678113f51a14173a2a70cba96d36b7ed054
PlaguedPlatypus <plaguedplatypus@gmail.com>
2026-06-08 15:54:35 -0700

    color tracking updates

--- main.js
+++ main.js
@@ -5195,20 +5195,17 @@ var appName = "ResourceTracker";
 var appColor = alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(0, 255, 0);
 var timestampRegex = /\[\d{2}:\d{2}:\d{2}\]/g;
 var reader = new (alt1_chatbox__WEBPACK_IMPORTED_MODULE_1___default())();
-reader.readargs.colors.push(alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(0, 255, 0), // Bright green
-// I'll find that green eventually
-// group 1
-alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(43, 97, 26), alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(39, 76, 26), alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(59, 177, 30), alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(50, 130, 28), 
-// group 2
-alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(57, 168, 29), alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(43, 162, 18), alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(43, 162, 18), alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(39, 94, 23), 
-// group 3
-alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(40, 114, 22), alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(34, 77, 21), alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(50, 200, 20), 
-// group 4
-alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(50, 138, 27), alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(42, 88, 26), alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(45, 108, 26), alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(54, 147, 29), alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(75, 33, 99), // what's this?
+reader.readargs.colors.push(
+// why does this game hate colors so much
+alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(0, 255, 0), // Bright green
+alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(50, 200, 20), // Carpet dust green
+alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(59, 181, 30), // alt1 hates this color or font for some reason
+alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(232, 47, 47), // You missed that seren spirit btw...
+alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(161, 53, 235), // what's this?
 alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(255, 153, 0), // Bright orange
 alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(255, 128, 0), // Medium orange
-alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(255, 112, 0), // Darker orange
-alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(255, 102, 0) // Even darker orange
+alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(255, 111, 0), // Darker orange
+alt1__WEBPACK_IMPORTED_MODULE_0__.mixColor(255, 140, 56) // pale orange
 );
 function getTimeStamp() {
     return new Date().toLocaleTimeString("en-US", {
--- src/index.ts
+++ src/index.ts
@@ -52,34 +52,18 @@ const timestampRegex = /\[\d{2}:\d{2}:\d{2}\]/g;
 const reader = new ChatboxReader();
 
 reader.readargs.colors.push(
+	// why does this game hate colors so much
 	a1lib.mixColor(0, 255, 0), // Bright green
-	// I'll find that green eventually
-	// group 1
-	a1lib.mixColor(43, 97, 26),
-	a1lib.mixColor(39, 76, 26),
-	a1lib.mixColor(59, 177, 30),
-	a1lib.mixColor(50, 130, 28),
-	// group 2
-	a1lib.mixColor(57, 168, 29),
-	a1lib.mixColor(43, 162, 18),
-	a1lib.mixColor(43, 162, 18),
-	a1lib.mixColor(39, 94, 23),
-	// group 3
-	a1lib.mixColor(40, 114, 22),
-	a1lib.mixColor(34, 77, 21),
-	a1lib.mixColor(50, 200, 20),
-	// group 4
-	a1lib.mixColor(50, 138, 27),
-	a1lib.mixColor(42, 88, 26),
-	a1lib.mixColor(45, 108, 26),
-	a1lib.mixColor(54, 147, 29),
+	a1lib.mixColor(50, 200, 20), // Carpet dust green
+	a1lib.mixColor(59, 181, 30), // alt1 hates this color or font for some reason
+	a1lib.mixColor(232, 47, 47), // You missed that seren spirit btw...
 
-	a1lib.mixColor(75, 33, 99), // what's this?
+	a1lib.mixColor(161, 53, 235), // what's this?
 
 	a1lib.mixColor(255, 153, 0), // Bright orange
 	a1lib.mixColor(255, 128, 0), // Medium orange
-	a1lib.mixColor(255, 112, 0), // Darker orange
-	a1lib.mixColor(255, 102, 0) // Even darker orange
+	a1lib.mixColor(255, 111, 0), // Darker orange
+	a1lib.mixColor(255, 140, 56) // pale orange
 );
 
 function getTimeStamp() {
